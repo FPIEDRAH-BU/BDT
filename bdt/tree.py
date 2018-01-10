4eee5b647eb74728cf73dd3af6e0ebe5d9b052fe
Francisco J. Piedrahita <fpiedrah93@gmail.com>
2018-01-10 14:17:14 -0500

    Added iterator to the tree structure

--- bdt/tree.py
+++ bdt/tree.py
@@ -19,5 +19,27 @@ class BDT(object):
     def __len__(self):
         return count(self)
 
+    def __iter__(self):
+        if not self.params_set:
+            raise UnboundLocalError(""" You should set up parameters first
+                                        use the set_parameters function. """)
 
+        return self
 
+    def next(self):
+        node = self.current_node
+
+        if not node:
+            raise StopIteration
+
+        if node.run_function(**self.params):
+            self.current_node = node.true_child
+        else:
+            self.current_node = node.false_child
+
+        return node
+
+    def set_parameters(self,
+                       params):
+        self.params_set = True
+        self.params = params
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -33,3 +33,94 @@ def test_len():
     )
 
     assert len(tree) == 3
+
+
+def test_iterator_true():
+    true_node = Node(
+        'True Node'
+    )
+
+    false_node = Node(
+        'False Node'
+    )
+
+    head_node = Node(
+        'Head Node',
+        lambda var: var < 10,
+        true_child=true_node,
+        false_child=false_node
+    )
+
+    tree = BDT(
+        head_node
+    )
+
+    tree.set_parameters({
+        'var': 5
+    })
+
+    real_path = []
+    path = ['Head Node',
+            'True Node']
+    for node in tree:
+        real_path.append(node.name)
+
+    assert path == real_path
+
+
+def test_iterator_false():
+    true_node = Node(
+        'True Node'
+    )
+
+    false_node = Node(
+        'False Node'
+    )
+
+    head_node = Node(
+        'Head Node',
+        lambda var: var < 10,
+        true_child=true_node,
+        false_child=false_node
+    )
+
+    tree = BDT(
+        head_node
+    )
+
+    tree.set_parameters({
+        'var': 100
+    })
+
+    real_path = []
+    path = ['Head Node',
+            'False Node']
+    for node in tree:
+        real_path.append(node.name)
+
+    assert path == real_path
+
+
+def test_iterator_error():
+    true_node = Node(
+        'True Node'
+    )
+
+    false_node = Node(
+        'False Node'
+    )
+
+    head_node = Node(
+        'Head Node',
+        lambda var: var < 10,
+        true_child=true_node,
+        false_child=false_node
+    )
+
+    tree = BDT(
+        head_node
+    )
+
+    with pytest.raises(UnboundLocalError):
+        for node in tree:
+            pass
