abad5a9117decb38e8c2a28e2ef6de21c6811ebf
Francisco J. Piedrahita <fpiedrah93@gmail.com>
2018-01-10 13:47:36 -0500

    Added node count functionality

--- bdt/tree.py
+++ bdt/tree.py
@@ -1,3 +1,13 @@
+def count(tree):
+    if tree.head == None:
+        return 0
+
+    true_child = tree.head.true_child
+    false_child = tree.head.false_child
+
+    return 1 + count(BDT(true_child)) + count(BDT(false_child))
+
+
 class BDT(object):
 
     def __init__(self,
@@ -5,3 +15,9 @@ class BDT(object):
         self.head = head
         self.current_node = head
         self.params_set = False
+
+    def __len__(self):
+        return count(self)
+
+
+
